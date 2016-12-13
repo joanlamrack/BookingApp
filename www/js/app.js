6c6c7aaf558901368e88845f61c3f342bae04ee0
joanlamrack <joanalamrack@gmail.com>
2016-12-13 11:45:03 +0700

    about inserted

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,3 +29,19 @@ function ContentController($scope, $ionicSideMenuDelegate) {
   };
 }
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'home.html'
+  })
+  .state('music', {
+    url: '/about',
+    templateUrl: 'about.html'
+  });
+  $urlRouterProvider.otherwise('/');  
+});
+
+app.controller('MainCtrl', function($scope) {
+ 
+});
